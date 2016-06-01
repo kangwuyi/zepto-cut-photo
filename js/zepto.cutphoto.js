f974a560e49b4a9fa4068c3a027cdb3d6218952a
kahn1990 <kangjian1990@live.com>
2016-06-01 17:21:49 +0800

    删掉多余代码

--- js/zepto.cutphoto.js
+++ js/zepto.cutphoto.js
@@ -5,15 +5,11 @@
         var options = cutPhoto_options.defaults;
 
         $("#" + data.browse_button).bind("change", function (event) {
-            //通过 this.files 取到 FileList ，这里只有一个
-            //通过file.size可以取得图片大小
-            $.showPreloader('加载中，请稍候');
             var reader = new FileReader();
             $(imgCutPreview).css({"width": "", "height": ""});
 
             reader.onload      = function (evt) {
                 document.getElementById(imgCutPreview.attr('id')).src = evt.target.result;
-                $.hidePreloader();
             };
             options.cutImgData = reader.readAsDataURL(this.files[0]);
         });
@@ -83,19 +79,13 @@
                 event.preventDefault() && event.stopPropagation();
                 options.moveEndX1 = event.changedTouches[0].pageX;
                 options.moveEndY1 = event.changedTouches[0].pageY;
-                //--计算是否发生位移，根据位移来定位裁剪框位置。
-                //位移量。
-                //--当前裁剪框原始位置。
                 options.cutLeft += (options.moveEndX1 - options.moveBeginX1);
                 options.cutTop += (options.moveEndY1 - options.moveBeginY1);
-                //--判断是否在矩形边框，假如超出去，那么就取最终点。
-                //--注意，判断相关点的范围。
                 if (options.cutLeft < options.cutBoxLimitX1) {
                     options.cutLeft = options.cutBoxLimitX1;
                 } else if (options.cutLeft > options.cutBoxLimitX2) {
                     options.cutLeft = options.cutBoxLimitX2;
                 }
-                //--顺便判断，加上宽度后，是否超过了范围。
                 if ((options.cutLeft + options.cutViewWidth) > options.cutBoxLimitX2) {
                     options.cutLeft = options.cutBoxLimitX2 - options.cutViewWidth;
                 }
@@ -104,12 +94,10 @@
                 } else if (options.cutTop > options.cutBoxLimitY2) {
                     options.cutTop = options.cutBoxLimitY2;
                 }
-                //--顺便判断，加上裁剪框高度后，是否超过下限。
                 if ((options.cutTop + options.cutViewHeight) > options.cutBoxLimitY2) {
                     options.cutTop = options.cutBoxLimitY2 - options.cutViewHeight;
                 }
                 cutBoxWidthAndHeight(true);
-                //---将这一点的放回前一点。
                 options.moveBeginX1 = options.moveEndX1;
                 options.moveBeginY1 = options.moveEndY1;
             }).bind("touchend", function (event) {
@@ -196,7 +184,6 @@
                 }
                 options.processEndX = event.changedTouches[0].pageX;
                 options.processEndY = event.changedTouches[0].pageY;
-                //--计算比分比。
                 options.processPercent += parseInt((options.processEndX - options.processBeginX) * 100 / options.processBarWidth);
                 if (options.processPercent < 0) {
                     options.processPercent = 0;
@@ -204,29 +191,23 @@
                 else if (options.processPercent > 100) {
                     options.processPercent = 100;
                 }
-                //--计算那个指示点位置。
                 options.processPointX = parseInt(options.processBarWidth * (options.processPercent / 100));
                 processPoint.css("left", options.processPointX + "px");
-                //--根据百分比，设置裁剪框大小。
                 var _new_cut_width  = parseInt(options.cutMaxWidth * (options.processPercent / 100)),
                     _new_cut_height = parseInt(options.cutMaxHeight * (options.processPercent / 100));
                 if (_new_cut_width > options.cutViewWidth) {
-                    //--扩大了。
-                    //--计算当前坐标
                     options.cutLeft       = options.cutLeft - parseInt((_new_cut_width - options.cutViewWidth) / 2);
                     options.cutTop        = options.cutTop - parseInt((_new_cut_height - options.cutViewHeight) / 2);
                     options.cutViewWidth  = _new_cut_width;
                     options.cutViewHeight = _new_cut_height;
                     cutBoxWidthAndHeight(true);
                 } else if (_new_cut_width < options.cutViewWidth) {
-                    //--缩小了。
                     options.cutLeft       = options.cutLeft + parseInt((options.cutViewWidth - _new_cut_width) / 2);
                     options.cutTop        = options.cutTop + parseInt((options.cutViewHeight - _new_cut_height) / 2);
                     options.cutViewWidth  = _new_cut_width;
                     options.cutViewHeight = _new_cut_height;
                     cutBoxWidthAndHeight(true);
                 }
-                //--后续处理。
                 options.processBeginX = options.processEndX;
                 options.processBeginY = options.processEndY;
             }).bind("touchend", function (event) {
@@ -349,12 +330,10 @@
 
         callback(function () {
             var output    = document.createElement("canvas"),
-                //--坐标换算。
                 scale_x   = options.cutImgWidth / options.cropViewWidth,
                 scale_y   = options.cutImgHeight / options.cropViewHeight,
                 _o_x      = parseInt((scale_x) * options.cutLeft),
                 _o_y      = parseInt((scale_y) * options.cutTop),
-                //--长度换算
                 _o_width  = parseInt(scale_x * options.cutViewWidth),
                 _o_height = parseInt(scale_y * options.cutViewHeight);
 
@@ -370,38 +349,35 @@
             height            : 300,
             cutWidth          : 300,
             cutHeight         : 300,
-            cutMinSize        : 50,//裁剪框最小尺寸，即最小可以缩放到这个size，width及height任意一个都无法小于这个值。
-            //--系统自带，运行时自动运算，请不要修改。
-            cropViewWidth     : 0,//在画布里面显示的最大宽度
-            cropViewHeight    : 0,//在画布里面显示的最大高度
+            cutMinSize        : 50,
+            cropViewWidth     : 0,
+            cropViewHeight    : 0,
             cropViewInitWidth : 0,
             cropViewInitHeight: 0,
             cropLeft          : 0,
             cropTop           : 0,
-            //--裁剪框
-            cutViewWidth      : 0,   //当前宽度，
-            cutViewHeight     : 0,//当前高度
-            cutMaxWidth       : 0,   //裁剪框最大宽度。
-            cutMaxHeight      : 0,//裁剪框最大高度。
-            //--四象限。用于判断距离。
+            cutViewWidth      : 0,
+            cutViewHeight     : 0,
+            cutMaxWidth       : 0,
+            cutMaxHeight      : 0,
             cutBoxLimitX1     : 0,
             cutBoxLimitX2     : 0,
             cutBoxLimitY1     : 0,
             cutBoxLimitY2     : 0,
-            cutLeft           : 0,//裁剪框绝对定位，左侧距离。
-            cutTop            : 0,//裁剪框绝对定位，离顶部距离。
-            initStatus        : false,//当前组件是否已经初始化了。
+            cutLeft           : 0,
+            cutTop            : 0,
+            initStatus        : false,
             cutImgWidth       : 0,
             cutImgHeight      : 0,
             cutImgData        : "",
-            processBeginX     : 0,//触摸时候起始点
-            processBeginY     : 0,//触摸时候起始点
-            processEndX       : 0,//触摸时候终点
-            processEndY       : 0,//触摸时候终点
-            processBarWidth   : 200,//进度条长度
-            processPointX     : 0,//当前指示点位置
+            processBeginX     : 0,
+            processBeginY     : 0,
+            processEndX       : 0,
+            processEndY       : 0,
+            processBarWidth   : 200,
+            processPointX     : 0,
             processPointY     : 0,
-            processPercent    : 0,//百分比值。
+            processPercent    : 0,
             processInitStatus : false
         }
     };
